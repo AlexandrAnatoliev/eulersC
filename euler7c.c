1c84ed4918934e37242483ddf3d35a133b04ceaf
AlexandrAnatoliev <per-1986@list.ru>
2023-06-06 17:17:03 +0300

    euler7c

---
+++ euler7c.c
@@ -0,0 +1,35 @@
+//euler7c - 10001-е простое число
+//Выписав первые шесть простых чисел, получим 2, 3, 5, 7, 11 и 13. Очевидно, что 6-е простое число - 13.
+//Какое число является 10001-м простым числом?
+
+#include <stdio.h>
+#include <stdbool.h>
+#include<math.h>
+
+bool isSimpleNum(int num_arg)
+{
+    int num = sqrt(num_arg) + 1;                //не нужно доходить до числа A A=a*a. Достаточно дойти до a
+    for (int i = 2; i < num; i++) 
+    {        
+        if (num_arg % i == 0) return false;     //% может работать только с целочисленными значениями
+    }
+    return true;
+}
+
+int main(void)
+{
+    int count = 0;          //10001
+    int answ = 0;           //104743
+    int num = 1;
+    while (count < 10001)    
+    {
+        num++;
+        if (isSimpleNum(num))
+        {
+            answ = num;
+            count++;
+        }
+    }
+    return 0;  
+}
+
