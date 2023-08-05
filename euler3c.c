f7737ea1191c9f2dbdead0cf7a50551e235ce4b5
AlexandrAnatoliev <per-1986@list.ru>
2023-08-05 19:44:53 +0300

    выложил в статью

--- euler3c.c
+++ euler3c.c
@@ -1,42 +1,42 @@
 #include <stdio.h>
-#include<math.h>
+#include <math.h>
+#include <stdbool.h>
 
 //euler3c - Наибольший простой делитель
 //Простые делители числа 13195 - это 5, 7, 13 и 29.
 //Каков самый большой делитель числа 600851475143, являющийся простым числом?
 
-int isSimple(long long num_arg)
-//если число простое, то возвращает 1, иначе - 0
+bool isSimple(long long num_arg)
+//если число простое, то возвращает true, иначе - false
 {
-    //проскольку число А можно предствить как квадрат a^2, множители можно проверять до а
+    //поскольку число N можно предствить как квадрат n^2, множители можно проверять до n
     int n = round(sqrt(num_arg) + 1);       
     for (int i = 2; i < n; i++)
     {
-        if (num_arg%i == 0) return 0;
+        if (num_arg%i == 0) 
+            return false;
     }        
-    return 1;
+    return true;
 }
 
 long long getMaxDiv(long long num_arg)
 //возвращает максимальный делитель числа
 {
     int j = 2;
-    while (num_arg%j != 0) j++;
-    long long answer = round(num_arg/j);
+    while (num_arg%j != 0) 
+        j++;
+    long long answer = num_arg/j;
     return answer;
 }
 
 int main(void)
 {
-    int res;
     long long answer = 600851475143;
 
-    answer = getMaxDiv(answer);
-    res = isSimple(answer);
-    while (res == 0)
+    answer = getMaxDiv(answer);    
+    while (isSimple(answer) == false)
     {
-         answer = getMaxDiv(answer);
-         res = isSimple(answer);         
+         answer = getMaxDiv(answer);                 
     }      
     printf("%lld\n", answer);
     return 0;
