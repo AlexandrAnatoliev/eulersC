f1e26fd268181660ff0d543734fc0a063c021427
AlexandrAnatoliev <per-1986@list.ru>
2024-02-25 21:30:18 +0300

    сайт

--- euler25c.c
+++ euler25c.c
@@ -22,38 +22,195 @@
 // Каков порядковый номер первого члена последовательности Фибоначчи, содержащего 1000 цифр?
 
 #include <stdio.h>
+#include <time.h>                                               // for clock_t, clock(), CLOCKS_PER_SEC
+#include <locale.h>                                             // русский язык в printf()
+#include <stdbool.h>
+
+#define LEN_NUMB 9                                              // int -> 10-значные числа => одна ячейка будет вмещать 9 цифр
+#define LEN_ARR 1000 / LEN_NUMB + 1                             // 1000 / 9 - количество ячеек в массиве
+
+typedef enum priority
+{
+    prev,
+    next
+} prior_t;                                                      // очередность чисел Фибоначчи (предыдущее, последующее)
+
+typedef struct big_num
+{
+    int bg_arr[LEN_ARR];                                        // храним число в виде массива девятизначных чисел [999999999,999999999...]
+    int lenght;                                                 // количество цифр в числе
+    int indx;                                                   // порядковый номер числа
+    prior_t priority;                                           // порядок чисел относительно друг друга
+} big_num_t;
+
+int big_num_init(big_num_t *big_num, int num, int index, prior_t prior);
+int get_len_num(int num);
+int get_next_fib(big_num_t *fib_prev, big_num_t *fib_next);
+bool input_more_limit(const char *name_func, const char *name_input, int input_value, const char *name_limit, int limit_value);
 
 int main(void)
 {
-    char arr_f1[1001] = {1};                        //первое число Фибоначчи
-    char arr_f2[1001] = {1};                        //второе
-    char arr_fnext[1001] = {0};                     //следующее - сумма первых двух
-    char num;
-    char dec = 0;
-    int cnt = 1;                                    //длина чисел
-    int f_num = 2;                                  //порядковый номер вычисленного числа Фибоначчи
-    
-    while(cnt < 1000)
+    double time_spent = 0.0;                                    // для хранения времени выполнения кода
+    clock_t begin = clock();                                    // СТАРТ таймера
+
+    setlocale(LC_ALL, "Rus");                                   // русский язык в printf()
+
+    int answ;
+
+    big_num_t fib1;
+    big_num_t fib2;
+
+    big_num_init(&fib1, 1, 1, prev);                            // первое число Фибоначчи
+    big_num_init(&fib2, 1, 2, next);                            // второе
+
+    while (fib1.lenght < 1000 && fib2.lenght < 1000)            // пока одно из чисел не достигло требуемой длины
     {
-        dec = 0;
-        for(int i = 0; i < cnt; i++)
-         {
-             num = arr_f1[i] + arr_f2[i] + dec;     //складываем поразрядно числа             
-             arr_fnext[i] = num%10;                 //разделяем первый разряд
-             dec = num/10;                          //и второй - если есть
-             arr_f1[i] = arr_f2[i];                 //обновляем первое число Фибоначчи
-             arr_f2[i] = arr_fnext[i];              //обновляем второе
-         }
-         
-        if (dec)                                    //если число не влезло в текущую разрядность
-        {
-            arr_f2[cnt] = dec;                      //записываем последнюю цифру в число
-            cnt++;                                  //увеличиваем разрядность чисел
-        }
-        f_num++;
-             
+        if (fib1.priority == prev && fib2.priority == next)     // складываем и меняем fib1 и fib2 местами в зависимости от их очередности
+            get_next_fib(&fib1, &fib2);
+        else
+            get_next_fib(&fib2, &fib1);
     }
 
-    printf("%d", f_num);                              //4782
+    answ = (fib1.lenght > fib2.lenght) ? fib1.indx : fib2.indx; // ответ - индекс большего числа
+
+    clock_t end = clock();                                      // СТОП таймера
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;       // время работы в секундах
+
+    printf("Ответ = %d время = %f\n", answ, time_spent);        // выводим результат и время работы программы
     return 0;
 }
+
+int big_num_init(big_num_t *big_num, int num, int index, prior_t prior)
+// функция задает начальные значения числа
+// параметры:	fib     - число Фибоначчи
+//              num     - начальное значение числа
+//              index   - порядковый номер числа
+//              prior   - очередность числа
+{
+    for (int i = 0; i < LEN_ARR; i++)                           // обнуляем массив
+        big_num->bg_arr[i] = 0;
+
+    big_num->bg_arr[0] = num;
+    big_num->indx = index;
+    big_num->priority = prior;
+    big_num->lenght = get_len_num(num);
+
+    return 0;
+}
+
+int get_len_num(int num)
+// функция получает количество цифр в числе
+// параметры:   num - число
+// return:          - количество цифр в числе
+{
+    int cnt = 0;
+
+    while (num)
+    {
+        num /= 10;
+        cnt++;
+    }
+
+    return cnt;
+}
+
+int get_next_fib(big_num_t *fib_prev, big_num_t *fib_next)
+// функция получает следующее значение числа Фибоначчи
+// параметры:	fib_prev    - число Фибоначчи
+//              fib_next    - следующее за ним число
+{
+    if (fib_prev->priority != prev || fib_next->priority != next)
+    {
+        printf("get_next_fib(): В %d-м или %d-м числах не корректно заданы поля \"priority\"\n", fib_prev->indx, fib_next->indx);
+        return 1;
+    }
+
+    int len_arr = fib_next->lenght / LEN_NUMB + 1;                                  // минимально необходимая длина массива для хранения числа
+    int residue = 0;
+
+    if (input_more_limit("get_next_fib()", "len_arr", len_arr, "LEN_ARR", LEN_ARR)) // обрабатываем выход за пределы массива
+        return 1;
+
+    for (int i = 0; i < len_arr; i++)
+    {
+        int sum_dig = (fib_prev->bg_arr[i] + fib_next->bg_arr[i]) + residue;        // складываем массивы по одной ячейке и переносим остаток
+        fib_prev->bg_arr[i] = sum_dig % 1000000000;                                 // сохраняем в fib_prev сумму fib_prev и fib_next
+        residue = sum_dig / 1000000000;
+    }
+
+    if (residue) // остался остаток
+    {
+        fib_prev->lenght = len_arr * LEN_NUMB + get_len_num(residue);               // определяем количество цифр в числе
+        fib_prev->bg_arr[len_arr] = residue;
+    }
+    else
+        fib_prev->lenght = (len_arr - 1) * LEN_NUMB + get_len_num(fib_prev->bg_arr[len_arr - 1]);
+
+    fib_prev->indx += 2;                                                            // меняем порядковый номер числа
+    fib_prev->priority = next;                                                      // и порядок чисел относительно друг друга
+    fib_next->priority = prev;
+
+    return 0;
+}
+
+bool input_more_limit(const char *name_func, const char *name_input, int input_value, const char *name_limit, int limit_value)
+// функция определяет превышение переменной своего предельного значения и выводит предупреждение
+// параметры:   *name_func  - название функции
+//              input_value - значение переменной
+//              *name_input - название переменной
+//              limit_value - предельное значение
+//              *name_limit - название предельного значения
+// return:      true        - при превышении
+{
+    if (input_value > limit_value)                                                  // обрабатываем превышение
+    {
+        printf("%s: %s = %d more %s = %d\n", name_func, name_input, input_value, name_limit, limit_value);
+        return true;
+    }
+
+    return false;
+}
+
+// #include <stdio.h>
+// #include <time.h>                                           // for clock_t, clock(), CLOCKS_PER_SEC
+
+// int main(void)
+// {
+//     double time_spent = 0.0;                                // для хранения времени выполнения кода
+//     clock_t begin = clock();                                // СТАРТ таймера
+
+//     char arr_f1[1001] = {1};                        //первое число Фибоначчи
+//     char arr_f2[1001] = {1};                        //второе
+//     char arr_fnext[1001] = {0};                     //следующее - сумма первых двух
+//     char num;
+//     char dec = 0;
+//     int cnt = 1;                                    //длина чисел
+//     int f_num = 2;                                  //порядковый номер вычисленного числа Фибоначчи
+
+//     while(cnt < 1000)
+//     {
+//         dec = 0;
+//         for(int i = 0; i < cnt; i++)
+//          {
+//              num = arr_f1[i] + arr_f2[i] + dec;     //складываем поразрядно числа
+//              arr_fnext[i] = num%10;                 //разделяем первый разряд
+//              dec = num/10;                          //и второй - если есть
+//              arr_f1[i] = arr_f2[i];                 //обновляем первое число Фибоначчи
+//              arr_f2[i] = arr_fnext[i];              //обновляем второе
+//          }
+
+//         if (dec)                                    //если число не влезло в текущую разрядность
+//         {
+//             arr_f2[cnt] = dec;                      //записываем последнюю цифру в число
+//             cnt++;                                  //увеличиваем разрядность чисел
+//         }
+//         f_num++;
+
+//     }
+
+//     clock_t end = clock();                                  // СТОП таймера
+//     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;   // время работы в секундах
+
+//     printf("answer = %d runtime = %f\n", f_num, time_spent);  // выводим результат и время работы программы
+//     return 0;
+// }
--- test.c
+++ test.c
@@ -1,170 +1,38 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <time.h> // for clock_t, clock(), CLOCKS_PER_SEC
+#include <locale.h>
+#include <stdlib.h>
 
-#define LEN_ARR 102
-
-int mark_facrors(char arr[], int start, int finish);
-bool input_more_limit(const char *name_func, const char *name_input, int input_value, const char *name_limit, int limit_value);
-int reduse_same_num(char arr1[], int cnt_arr1, char arr2[], int cnt_arr2);
-bool is_answer(char n_div_r_arr[], int cnt_n_div_r, char n_munus_r_arr[], int cnt_n_munus_r);
+int* twoSum(int* nums, int numsSize, int target, int* returnSize);
 
 int main(void)
 {
-    double time_spent = 0.0; // для хранения времени выполнения кода
-    clock_t begin = clock(); // СТАРТ таймера
-
-    int cnt = 0;
-
-    for (int r = 2; r <= 63; r++)
-    {
-        for (int n = r + 1; n <= 63; n++)
-        {
-            char n_div_r_arr[LEN_ARR] = {0}; // n! / r!
-            char n_munus_r_arr[LEN_ARR] = {0};
-            int n_div_r = mark_facrors(n_div_r_arr, r + 1, n + 1);
-            int n_minus_r = mark_facrors(n_munus_r_arr, 1, n - r + 1);
-            int same = reduse_same_num(n_div_r_arr, n_div_r, n_munus_r_arr, n_minus_r);
-            cnt += is_answer(n_div_r_arr, n_div_r - same, n_munus_r_arr, n_minus_r - same);
-        }
-    }
-    clock_t end = clock();                                // СТОП таймера
-    time_spent += (double)(end - begin) / CLOCKS_PER_SEC; // время работы в секундах
-
-    printf("answers = %d runtime = %f\n", cnt, time_spent); // выводим результат и время работы программы
-
+    setlocale(LC_ALL, "Rus");
+    int *answ;
+    int arr[] = {2,7,11,15};
+    answ = twoSum(arr,4,18, 0);
+    int x = answ[1];
     return 0;
 }
 
-int mark_facrors(char arr[], int start, int finish)
-// функция отмечает множители в массиве по их индексу: arr[num] = 1
-// параметры:	arr[]   - массив для хранения множителей
-//              start   - с этого числа отмечает
-//              finish  - до этого числа отмечает
-// return:              - количество занесенных цифр в массив
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) 
 {
-    if (input_more_limit("mark_factor()", "finish", finish, "LEN_ARR", LEN_ARR)) // обрабатываем выход за пределы массива
-        return 0;
-    if (input_more_limit("mark_factor()", "start", start, "finish", finish)) // обрабатываем корректность значений start и finifh
-        return 0;
-
-    int cnt = 0;
-    for (int i = start; i < finish; i++, cnt++)
-        arr[i] = 1;
-    return cnt;
-}
-
-bool input_more_limit(const char *name_func, const char *name_input, int input_value, const char *name_limit, int limit_value)
-// функция определяет превышение переменной своего предельного значения и выводит предупреждение
-// параметры:   *name_func  - название функции
-//              input_value - значение переменной
-//              *name_input - название переменной
-//              limit_value - предельное значение
-//              *name_limit - название предельного значения
-// return:      true        - при превышении
-{
-    if (input_value > limit_value) // отрабатываем превышение
+    *returnSize = 2;
+    int *answ = malloc(*returnSize * sizeof(int));
+    for(int i = 0; i < numsSize - 1; i++)
     {
-        printf("%s: %s = %d more %s = %d\n", name_func, name_input, input_value, name_limit, limit_value);
-        return true;
-    }
-
-    return false;
-}
-
-int reduse_same_num(char arr1[], int cnt_arr1, char arr2[], int cnt_arr2)
-// функция сокращает одинаковые множители в массивах по их индексу
-// параметры:	arr1[]      - массив 1 для хранения множителей
-//              cnt_arr1    - количество множителей в нем
-//              arr2[]      - массив 2 для хранения множителей
-//              cnt_arr1    - количество множителей в нем
-// return:                  - количество сокращенных цифр
-{
-    if (input_more_limit("reduse_same_num()", "cnt_arr1", cnt_arr1, "LEN_ARR", LEN_ARR)) // в массиве не может быть больше элементов, чем его длина
-        return 0;
-    if (input_more_limit("reduse_same_num()", "cnt_arr2", cnt_arr2, "LEN_ARR", LEN_ARR))
-        return 0;
-
-    int cnt = 0;
-
-    for (int i = 0; i < LEN_ARR; i++)
-    {
-        if (arr1[i]) // считаем пройденные множители в массивах
-            cnt_arr1--;
-        if (arr2[i])
-            cnt_arr2--;
-
-        if (arr1[i] && arr2[i]) // сокращаем множители, совпадающие в двух массивах
+        for(int j = i + 1; j < numsSize; j++)
         {
-            arr1[i] = 0;
-            arr2[i] = 0;
-            cnt++;
-        }
-        if (cnt_arr1 == 0 || cnt_arr2 == 0) // прерываем если в одном из массиво прошли по всем множителям
-            break;
-    }
-    return cnt;
-}
-
-bool is_answer(char n_div_r_arr[], int cnt_n_div_r, char n_munus_r_arr[], int cnt_n_munus_r)
-// функция перемножает множители числителя и делит на множители знаменателя
-// параметры:	n_div_r_arr[]   - массив для хранения множителей числителя
-//              cnt_n_div_r     - количество множителей в нем
-//              n_munus_r_arr[] - массив для хранения множителей знаменателя
-//              cnt_n_munus_r   - количество множителей в нем
-// return:      true        - количество выборок превышает один миллион
-{
-    if (input_more_limit("is_answer()", "cnt_n_div_r", cnt_n_div_r, "LEN_ARR", LEN_ARR)) // в массиве не может быть больше элементов, чем его длина
-        return 0;
-    if (input_more_limit("is_answer()", "cnt_n_munus_r", cnt_n_munus_r, "LEN_ARR", LEN_ARR))
-        return 0;
-
-    unsigned long long factor = 1;
-    int denom = 1;
-    int denom_num = 2;
-    int factor_num = 2;
-    cnt_n_munus_r--;
-
-    while ((cnt_n_munus_r || cnt_n_div_r) && (denom_num < LEN_ARR || factor_num < LEN_ARR))
-    {
-        if (denom_num < LEN_ARR && n_munus_r_arr[denom_num] == 0)
-            denom_num++; // ищем следующий множитель в знаменателе
-
-        if (denom_num < LEN_ARR && cnt_n_munus_r && n_munus_r_arr[denom_num])
-        {
-            denom *= denom_num; // увеличиваем знаменатель
-            cnt_n_munus_r--;
-            denom_num++;
-        }
-
-        while (factor_num < LEN_ARR && (factor % denom || (denom == 1 && cnt_n_munus_r == 0))) // если числитель не делится нацело на знаменатель
-        {
-            if (n_div_r_arr[factor_num] == 0)
-                factor_num++; // ищем следующий множитель числителя
-
-            if (cnt_n_div_r && n_div_r_arr[factor_num])
+            if(nums[i] + nums[j] == target)
             {
-                factor *= factor_num; // увеличиваем числитель
-                cnt_n_div_r--;
-                factor_num++;
+                answ[0] = i;
+                answ[1] = j;
+                return answ;
             }
         }
-
-        if (factor % denom == 0)
-        {
-            factor /= denom;
-            denom = 1;
-        }
-
-        if (cnt_n_munus_r == 0 && denom == 1 && factor > 1000000) // в знаменателе сократили все множители
-            return true;
-
-        if (cnt_n_munus_r == 0 && cnt_n_div_r == 0 && factor > 1000000) // перебрали все множители числителя и значенателя
-            return true;
-
-        if (cnt_n_div_r == 0 && factor <= 1000000)
-            return false;
     }
-
-    return false;
 }
