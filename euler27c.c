46ba06038dd2488069346d0369d2b44ab22bd1d6
AlexandrAnatoliev <per-1986@list.ru>
2024-03-12 21:52:05 +0300

    все работает

--- euler27c.c
+++ euler27c.c
@@ -1,88 +1,311 @@
 // euler27c - Квадратичные простые числа
 
-//Эйлер опубликовал свою замечательную квадратичную формулу :
+// Эйлер опубликовал свою замечательную квадратичную формулу :
 
-//n2 + n + 41
+// n2 + n + 41
 
-//Оказалось, что согласно данной формуле можно получить 40 простых чисел, последовательно подставляя значения 0≤n≤39
+// Оказалось, что согласно данной формуле можно получить 40 простых чисел, последовательно подставляя значения 0≤n≤39
 
-//Однако, при n = 40,
-// 402 + 40 + 41 = 40(40 + 1) + 41
-//делится на 41 без остатка, и, очевидно, при n = 41, 412 + 41 + 41
-//делится на 41 без остатка.
+// Однако, при n = 40,
+//  402 + 40 + 41 = 40(40 + 1) + 41
+// делится на 41 без остатка, и, очевидно, при n = 41, 412 + 41 + 41
+// делится на 41 без остатка.
 //
-//При помощи компьютеров была найдена невероятная формула 
+// При помощи компьютеров была найдена невероятная формула
 
 // n2−79n + 1601
 
 //, согласно которой можно получить 80 простых чисел для последовательных значений n
-//от 0 до 79. Произведение коэффициентов −79 и 1601 равно −126479.
+// от 0 до 79. Произведение коэффициентов −79 и 1601 равно −126479.
 
-//Рассмотрим квадратичную формулу вида :
+// Рассмотрим квадратичную формулу вида :
 
-//n2 + an + b
+// n2 + an + b
 //, где | a | < 1000
-//    и | b | ≤1000
+//     и | b | ≤1000
 //
-//    где | n |
-//    является модулем(абсолютным значением) n
-//    
-//    К примеру, | 11 |= 11 и | −4 |= 4
-//    Найдите произведение коэффициентов a и b квадратичного выражения, 
-// согласно которому можно получить максимальное количество простых чисел для последовательных значений n, начиная со значения n = 0
+//     где | n |
+//     является модулем(абсолютным значением) n
+//
+//     К примеру, | 11 |= 11 и | −4 |= 4
+//     Найдите произведение коэффициентов a и b квадратичного выражения,
+//  согласно которому можно получить максимальное количество простых чисел для последовательных значений n, начиная со значения n = 0
 
 #include <stdio.h>
-#include <stdbool.h>                                                //для красоты кода использую bool
-#include <math.h>                                                   //для использовании функции sqrt()
+#include <stdbool.h> //для красоты кода использую bool
+#include <math.h>    //для использовании функции sqrt()
+#include <time.h>    // for clock_t, clock(), CLOCKS_PER_SEC
+#include <stdlib.h>  // для calloc() и free()
+#include <locale.h>  // русский язык в printf()
 
-#define LEN_ARR 15000                                               //принял длину масиива опытным путем
+void *my_realloc(char *arr, int *len_arr, size_t len_add);
+int note_comp_arr(char *comp_arr, int start, int finish);
+bool is_composite(char comp_arr[], int num);
+int note_composite(char comp_arr[], int prime, int start, int finish);
+bool input_more_limit(const char *name_func, const char *name_input, int input_value, const char *name_limit, int limit_value);
 
-bool is_simple(int num_arg)
-//функция принимает число и возвращает true - если число простое
-{
-    int i_arg = sqrt(num_arg) + 1;                                  //выносим вычисление квадратного корня из цикла for
-
-    for (int i = 2; i < i_arg; i++)                                 //чтобы он не вычислялся каждую итерацию цикла
-    {
-        if (num_arg % i == 0)
-            return false;
-    }
-    return true;
-}
 int main(void)
 {
-    char arr[LEN_ARR] = {0};                                        //массив для записи простых чисел
+    double time_spent = 0.0; // для хранения времени выполнения кода
+    clock_t begin = clock(); // СТАРТ таймера
+
+    setlocale(LC_ALL, "Rus"); // русский язык в printf()
+
+    int len_arr = 1000; // длина массива
+    // size_t это typedef unsigned long long size_t - тип данных для sizeof
     int n, answ_a, answ_b, a, b, cnt, answ = 1, cnt_max = 0;
 
-    //вычисляю простые числа до 15000 один раз и заношу их массив по индексу
-    for (int i = 1; i < LEN_ARR; i++)                               //если arr[indx] = 1 => indx - простое число 
-        arr[i] = is_simple(i);
+    char *comp_arr = calloc(len_arr, sizeof(char)); // массив для хранения составных чисел
 
-    //в цикле перебираю все возможные коэффициенты
-    for (a = -1000; a < 1000; a++)
+    note_comp_arr(comp_arr, 2, len_arr); // заполняем массив составными числами
+    // for (int i = 9000; i < len_arr; i++)
+    //     if (!comp_arr[i])
+    //         printf("%d ", i);
+
+    // // в цикле перебираю все возможные коэффициенты
+    // for (a = -63; a < 1000; a++)
+    // {
+    //     for (b = 971; b < 1000; b++)
+    //     {
+    //         cnt = 0;
+    //         n = 0;
+    //         for (n = 0; n < 100; n++) // предположил, что простых чисел будет не более ста
+    //         {
+    //             answ = (n * n) + (a * n) + b;        // проверяем формулу
+    //             if (answ < 0 || comp_arr[answ] == 1) // простое число не может быть отрицательным
+    //                 break;                           // прервем проверку формулы если встретилось число не являющееся простым
+
+    //             cnt++;
+    //         }
+
+    //         if (cnt > cnt_max) // обновляем значения коэффициентов
+    //         {
+    //             cnt_max = cnt;
+    //             answ_a = a;
+    //             answ_b = b;
+    //         }
+    //     }
+    // }
+
+    // в цикле перебираю все возможные коэффициенты
+    for (int a = -1000; a < 1000; a++)
     {
-        for (b = -1000; b < 1000; b++)
+        for (int b = -1000; b < 1000; b++)
         {
-            cnt = 0;
-            n = 0;
-            for(n = 0; n < 100; n++)                                //предположил, что простых чисел будет не более ста
+            int cnt = 0;
+            for (int n = 0; n < 100; n++) // предположил, что простых чисел будет не более ста
             {
-                answ = (n * n) + (a * n) + b;                       //проверяем формулу
-                if (answ < 0 || arr[answ] == 0)                     //простое число не может быть отрицательным
-                    break;                                          //прервем проверку формулы если встретилось число не являющееся простым
-                            
+                int answ = (n * n) + (a * n) + b; // проверяем формулу
+
+                if (answ >= len_arr)
+                {
+                    int past_len = len_arr;                                           // сохраняем изначальную длину массива
+                    comp_arr = my_realloc(comp_arr, &len_arr, answ - len_arr + 1000); // т.к. len_arr  увеличивается в функции
+                    note_comp_arr(comp_arr, past_len, len_arr);                       // отмечаем в добавленной части массива составные числа
+                    printf("%d\n", len_arr);
+                }
+
+                if (answ < 0 || comp_arr[answ] == 1) // простое число не может быть отрицательным
+                    break;                           // прервем проверку формулы если встретилось число не являющееся простым
+
                 cnt++;
             }
 
-            if (cnt > cnt_max)                                      //обновляем значения коэффициентов
+            if (cnt > cnt_max) // обновляем значения коэффициентов
             {
                 cnt_max = cnt;
                 answ_a = a;
                 answ_b = b;
-            }            
+            }
         }
     }
-         
-    printf("cnt = %d\na*b = %d", cnt_max - 1, answ_a*answ_b);       //cnt = 70 a*b = -59231
+
+    free(comp_arr); // освобождаем место
+
+    clock_t end = clock();                                // СТОП таймера
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC; // время работы в секундах
+
+    printf("Ответ = %d время = %f\n", answ_a * answ_b, time_spent); // выводим результат и время работы программы
     return 0;
 }
+
+void *my_realloc(char *arr, int *len_arr, size_t len_add)
+// Функция увеличивает размер массива
+// Параметры:	arr[]   - массив с ранее вычисленными составными числами
+//              len_arr - старая длина массива
+//              len_add - на сколько увеличиваем массив
+// return:      указатель на увеличенный массив
+{
+    char *new_arr = calloc(*len_arr + len_add, sizeof(char)); // создаем увеличенный массив
+
+    if (new_arr == NULL)
+    {
+        printf("my_realloc: функция не смогла выделить место под массив");
+        return arr;
+    }
+
+    for (int i = 0; i < *len_arr; i++)
+        new_arr[i] = arr[i]; // копируем данные вновый массив из старого
+
+    free(arr);           // освобождаем память старого массива
+    arr = new_arr;       // присваиваем адрес нового массива
+    *len_arr += len_add; // перезаписываем длину массива
+
+    return arr;
+}
+
+int note_comp_arr(char *comp_arr, int start, int finish)
+// Функция отмечает составные числа в массиве
+// Параметры:	comp_arr[]  - массив с ранее вычисленными составными числами
+//              start       - начальное число
+//              finish      - конечное число
+{
+    // отмечаем составные числа на основе уже найденных простых
+    for (int num = 2; num < start; num++) // если число не отмечено составным (простое)
+        if (!comp_arr[num])
+            note_composite(comp_arr, num, start, finish); // отмечаем составные числа, кратные простому
+
+    // ищем новые простые числа и отмечаем составные
+    for (int num = start; num < finish; num++) // определяем список простых чисел (!составных)
+    {
+        // если число не отмечено составным -> проверяем число -> если число простое
+        if (!comp_arr[num] && !is_composite(comp_arr, num))
+            note_composite(comp_arr, num, start, finish); // отмечаем составные числа, кратные простому
+    }
+    return 0;
+}
+
+bool is_composite(char comp_arr[], int num)
+// функция принимает число и возвращает true - если число составное
+// параметры:	comp_arr[]          - массив с ранее вычисленными составными числами
+//              comp_arr[num] = 0   - число простое!
+//              comp_arr[num] = 1   - число составное!
+//              num                 - проверяемое число
+// return:      true                - если число составное!
+{
+    // if (num >= LEN_ARR || num < 0)          // проверка на выход числа за пределы массива
+    // {
+    //     printf("is_composite(): array index (num) = % d out of bounds [0 - %d]\n", num, LEN_ARR);
+    //     return false;
+    // }
+
+    int div_max = sqrt(num) + 1; // выносим вычисление квадратного корня из цикла for
+
+    for (int div = 2; div < div_max; div++) // чтобы он не вычислялся каждую итерацию цикла
+    {
+        if (!comp_arr[div] && !(num % div)) // пропускаем составные делители и срабатываем при num % i == 0
+            return true;
+    }
+
+    return false;
+}
+
+int note_composite(char comp_arr[], int prime, int start, int finish)
+// функция отмечает составные числа в массиве
+// параметры:	comp_arr[]          - массив с ранее вычисленными составными числами
+//              comp_arr[num] = 0   - число простое!
+//              comp_arr[num] = 1   - число составное!
+//              prime               - простое число, служит шагом
+//              finish              - конечный индекс в массиве
+// return:                          - количество отмеченных составных чисел
+{
+    // if (prime >= LEN_ARR + 1 || prime < 0)          // проверка на выход числа за пределы массива
+    // {
+    //     printf("note_composite(): array index (prime) = % d out of bounds [0 - %d]\n", prime, LEN_ARR);
+    //     return false;
+    // }
+
+    // if (finish >= LEN_ARR + 1 || finish < 0)        // проверка на выход числа за пределы массива
+    // {
+    //     printf("note_composite(): array index (finish) = % d out of bounds [0 - %d]\n", finish, LEN_ARR);
+    //     return false;
+    // }
+
+    start = (start > prime) ? start + (prime - start % prime) : 2 * prime; // первое число - простое, не отмечаем его
+
+    for (int i = start; i < finish; i += prime)
+        comp_arr[i] = 1; // отмечаем составные числа
+
+    return 0;
+}
+
+bool input_more_limit(const char *name_func, const char *name_input, int input_value, const char *name_limit, int limit_value)
+// функция определяет превышение переменной своего предельного значения и выводит предупреждение
+// параметры:   *name_func  - название функции
+//              input_value - значение переменной
+//              *name_input - название переменной
+//              limit_value - предельное значение
+//              *name_limit - название предельного значения
+// return:      true        - при превышении
+{
+    if (input_value > limit_value) // обрабатываем превышение
+    {
+        printf("%s: %s = %d more %s = %d\n", name_func, name_input, input_value, name_limit, limit_value);
+        return true;
+    }
+
+    return false;
+}
+
+// #include <stdio.h>
+// #include <stdbool.h>                                                //для красоты кода использую bool
+// #include <math.h>                                                   //для использовании функции sqrt()
+// #include <time.h>                                           // for clock_t, clock(), CLOCKS_PER_SEC
+
+// #define LEN_ARR 15000                                               //принял длину масиива опытным путем
+
+// bool is_simple(int num_arg)
+// //функция принимает число и возвращает true - если число простое
+// {
+//     int i_arg = sqrt(num_arg) + 1;                                  //выносим вычисление квадратного корня из цикла for
+
+//     for (int i = 2; i < i_arg; i++)                                 //чтобы он не вычислялся каждую итерацию цикла
+//     {
+//         if (num_arg % i == 0)
+//             return false;
+//     }
+//     return true;
+// }
+// int main(void)
+// {
+//     double time_spent = 0.0;                                // для хранения времени выполнения кода
+//     clock_t begin = clock();                                // СТАРТ таймера
+//     char arr[LEN_ARR] = {0};                                        //массив для записи простых чисел
+//     int n, answ_a, answ_b, a, b, cnt, answ = 1, cnt_max = 0;
+
+//     //вычисляю простые числа до 15000 один раз и заношу их массив по индексу
+//     for (int i = 1; i < LEN_ARR; i++)                               //если arr[indx] = 1 => indx - простое число
+//         arr[i] = is_simple(i);
+
+//     //в цикле перебираю все возможные коэффициенты
+//     for (a = -1000; a < 1000; a++)
+//     {
+//         for (b = -1000; b < 1000; b++)
+//         {
+//             cnt = 0;
+//             n = 0;
+//             for(n = 0; n < 100; n++)                                //предположил, что простых чисел будет не более ста
+//             {
+//                 answ = (n * n) + (a * n) + b;                       //проверяем формулу
+//                 if (answ < 0 || arr[answ] == 0)                     //простое число не может быть отрицательным
+//                     break;                                          //прервем проверку формулы если встретилось число не являющееся простым
+
+//                 cnt++;
+//             }
+
+//             if (cnt > cnt_max)                                      //обновляем значения коэффициентов
+//             {
+//                 cnt_max = cnt;
+//                 answ_a = a;
+//                 answ_b = b;
+//             }
+//         }
+//     }
+
+//     // printf("cnt = %d\na*b = %d", cnt_max - 1, answ_a*answ_b);
+//     clock_t end = clock();                                  // СТОП таймера
+//     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;   // время работы в секундах
+
+//     printf("Ответ = %d время = %f\n", answ_a*answ_b, time_spent);    // выводим результат и время работы программы
+//     return 0;
+// }
--- test.c
+++ test.c
@@ -1,42 +1,62 @@
 #include <stdio.h>
+#include <stdbool.h>                                                //для красоты кода использую bool
+#include <math.h>                                                   //для использовании функции sqrt()
+#include <time.h>                                           // for clock_t, clock(), CLOCKS_PER_SEC
 
-void copy_arr(double arr[], double sour[], int n);
-void copy_ptr(double arr[], double sour[], int n);
-void copy_ptrs(double arr[], double sour[], double *n);
+#define LEN_ARR 15000                                               //принял длину масиива опытным путем
 
+bool is_simple(int num_arg)
+//функция принимает число и возвращает true - если число простое
+{
+    int i_arg = sqrt(num_arg) + 1;                                  //выносим вычисление квадратного корня из цикла for
+
+    for (int i = 2; i < i_arg; i++)                                 //чтобы он не вычислялся каждую итерацию цикла
+    {
+        if (num_arg % i == 0)
+            return false;
+    }
+    return true;
+}
 int main(void)
 {
-    double sourse[2][5] = {{5, 6, 7, 8, 9}, {1, 2, 3, 4, 5}};
-    double arr1[2][5];
-    double arr2[5];
-    double arr3[5];
+    double time_spent = 0.0;                                // для хранения времени выполнения кода
+    clock_t begin = clock();                                // СТАРТ таймера
+    char arr[LEN_ARR] = {0};                                        //массив для записи простых чисел
+    int n, answ_a, answ_b, a, b, cnt, answ = 1, cnt_max = 0;
 
-    for (int i = 0, j = 1; i < 1; i++)
-        copy_ptrs(arr1[j], sourse[i], sourse[1]);
-    // copy_arr(arr1, sourse, 5);
-    // copy_ptr(arr2, sourse, 5);
-    // copy_ptrS(arr3, sourse, sourse + 5);
+    //вычисляю простые числа до 15000 один раз и заношу их массив по индексу
+    for (int i = 1; i < LEN_ARR; i++)                               //если arr[indx] = 1 => indx - простое число
+        arr[i] = is_simple(i);
+
+    //в цикле перебираю все возможные коэффициенты
+    for (a = -1000; a < 1000; a++)
+    {
+        for (b = -1000; b < 1000; b++)
+        {
+            cnt = 0;
+            n = 0;
+            for(n = 0; n < 100; n++)                                //предположил, что простых чисел будет не более ста
+            {
+                answ = (n * n) + (a * n) + b;                       //проверяем формулу
+                if (answ < 0 || arr[answ] == 0)                     //простое число не может быть отрицательным
+                    break;                                          //прервем проверку формулы если встретилось число не являющееся простым
+
+                cnt++;
+            }
+
+            if (cnt > cnt_max)                                      //обновляем значения коэффициентов
+            {
+                cnt_max = cnt;
+                answ_a = a;
+                answ_b = b;
+            }
+        }
+    }
+
+    // printf("cnt = %d\na*b = %d", cnt_max - 1, answ_a*answ_b);
+    clock_t end = clock();                                  // СТОП таймера
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;   // время работы в секундах
+
+    printf("Ответ = %d время = %f\n", answ_a*answ_b, time_spent);    // выводим результат и время работы программы
     return 0;
-}
-
-void copy_arr(double arr[], double sour[], int n)
-{
-    for (int i = 0; i < n; i++)
-        arr[i] = sour[i];
-}
-
-void copy_ptr(double arr[], double sour[], int n)
-{
-    double *ptr1 = arr;
-    double *prt2 = sour;
-    for (int i = 0; i < n; i++)
-        *(ptr1 + i) = *(sour + i);
-}
-
-void copy_ptrs(double arr[], double sour[], double *n)
-{
-    double *ptr1 = arr;
-    double *prt2 = sour;
-    while (sour != n)
-        *ptr1++ = *sour++;
 }
