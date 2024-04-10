9b01b196ad02e68e3ef56a5caa7b20c2fd7c86b1
AlexandrAnatoliev <per-1986@list.ru>
2024-04-10 22:24:35 +0300

    сайт

--- euler39c.c
+++ euler39c.c
@@ -3,43 +3,92 @@
 // Если p - периметр прямоугольного треугольника с целочисленными длинами сторон{ a,b,c }, то существует ровно три решения для p = 120:
 //
 // {20, 48, 52}, { 24,45,51 }, { 30,40,50 }
+//// 840
+// Какое значение p ≤ 1000 дает максимальное число решений ?
+
+// euler39c - Целые прямоугольные треугольники
+
+// Если p - периметр прямоугольного треугольника с целочисленными длинами сторон{ a,b,c }, то существует ровно три решения для p = 120:
 //
+// {20, 48, 52}, { 24,45,51 }, { 30,40,50 }
+//// 840
 // Какое значение p ≤ 1000 дает максимальное число решений ?
 
 #include <stdio.h>
-#include <stdbool.h>
+#include <time.h> 											// for clock_t, clock(), CLOCKS_PER_SEC
+#include <locale.h>
 
-bool is_right_angle(int a_arg, int b_arg, int c_arg)
-// Определяет, является ли треугольник прямогугольным
-// если a^2 + b^2 = c^2, то a, b, c - Пифагоровы числа
-// return:	true - треугольник прямогугольный
-{
-	return a_arg * a_arg + b_arg * b_arg == c_arg * c_arg;
-}
+#define MAX_PER 1000 										// максимальный периметр треугольника
 
 int main(void)
 {
+	setlocale(LC_ALL, "Rus"); 								// русский язык в printf()
+
+	double time_spent = 0.0; 								// для хранения времени выполнения кода
+	clock_t begin = clock(); 								// СТАРТ таймера
+
 	int answ = 0;
-	int cnt_max = 0;
-	int cnt = 0;
+	int answ_arr[MAX_PER + 1] = {0}; 						// для каждого периметра считаем решения
 
-	for (int p = 3; p <= 1000; p++)
+	static int sqrt_arr[MAX_PER * MAX_PER] = {0};
+	for (int i = 1; i < MAX_PER; i++) 						// заносим в массив квадратные корни натуральных чисел
+		sqrt_arr[i * i] = i;
+
+	for (int a = 1; a < MAX_PER; a++) 						// перебираем катеты a и b
 	{
-		cnt = 0;
-		for (int c = p / 2; c > 0; c--)							// p = a + b + c => т.к. a + b > c => c < p/2
+		for (int b = a; b < (MAX_PER - a); b++) 			// b > a чтобы катеты не повторялись
 		{
-			int a_plus_b = p - c;								// a_plus_b = a + b > c
-			for (int a = a_plus_b; a >= a_plus_b / 2; a--)		// a и b равнозначны, после ab/2 решения будут повторяться
-				if (is_right_angle(a, a_plus_b - a, c))
-					cnt++;		
-		}
+			int c = sqrt_arr[a * a + b * b]; 				// гипотенуза треугольника
 
-		if (cnt_max < cnt)
-		{
-			cnt_max = cnt;
-			answ = p;
+			if (c && (a + b + c) < MAX_PER) 				// если целочисленная и сумма сторон не превышает максимальный периметр
+				answ_arr[a + b + c]++;						// учитываем это решение для данного периметра
 		}
 	}
-	printf("%d", answ);											// 840
-	return 0;	
+
+	for (int perim = 0; perim < MAX_PER + 1; perim++) 		// ищем ответ в массиве
+		answ = (answ_arr[answ] > answ_arr[perim]) ? answ : perim;
+
+	clock_t end = clock();								  	// СТОП таймера
+	time_spent += (double)(end - begin) / CLOCKS_PER_SEC; 	// время работы в секундах
+
+	printf("Ответ = %d время = %f\n", answ, time_spent); 	// выводим результат и время работы программы
+	return 0;
 }
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// bool is_right_angle(int a_arg, int b_arg, int c_arg)
+// // Определяет, является ли треугольник прямогугольным
+// // если a^2 + b^2 = c^2, то a, b, c - Пифагоровы числа
+// // return:	true - треугольник прямогугольный
+// {
+// 	return a_arg * a_arg + b_arg * b_arg == c_arg * c_arg;
+// }
+
+// int main(void)
+// {
+// 	int answ = 0;
+// 	int cnt_max = 0;
+// 	int cnt = 0;
+
+// 	for (int p = 3; p <= 1000; p++)
+// 	{
+// 		cnt = 0;
+// 		for (int c = p / 2; c > 0; c--)							// p = a + b + c => т.к. a + b > c => c < p/2
+// 		{
+// 			int a_plus_b = p - c;								// a_plus_b = a + b > c
+// 			for (int a = a_plus_b; a >= a_plus_b / 2; a--)		// a и b равнозначны, после ab/2 решения будут повторяться
+// 				if (is_right_angle(a, a_plus_b - a, c))
+// 					cnt++;
+// 		}
+
+// 		if (cnt_max < cnt)
+// 		{
+// 			cnt_max = cnt;
+// 			answ = p;
+// 		}
+// 	}
+// 	printf("%d", answ);
+// 	return 0;
+// }
--- test.c
+++ test.c
@@ -1,68 +1,49 @@
+// euler39c - Целые прямоугольные треугольники
+
+// Если p - периметр прямоугольного треугольника с целочисленными длинами сторон{ a,b,c }, то существует ровно три решения для p = 120:
+//
+// {20, 48, 52}, { 24,45,51 }, { 30,40,50 }
+//// 840
+// Какое значение p ≤ 1000 дает максимальное число решений ?
+
 #include <stdio.h>
 #include <stdbool.h>
-#include <time.h> 													// for clock_t, clock(), CLOCKS_PER_SEC
 
-char get_arr(char arr_arg[], int num_arg, int div_arg)
-// раскладывает число на цифры по заданному основанию и заносит их в массив
-// параметры:
-// char arr_arg[]: массив, в который будет сохраняться разложенное число;
-// int num_arg: раскладываемое число;
-// int div_arg: основание, по которому раскладывается число
-// return: количество цифр в числе
+bool is_right_angle(int a_arg, int b_arg, int c_arg)
+// Определяет, является ли треугольник прямогугольным
+// если a^2 + b^2 = c^2, то a, b, c - Пифагоровы числа
+// return:	true - треугольник прямогугольный
 {
-	char cnt = 0;
-	while (num_arg)									//раскладываем число на цифры и заносим в массив
-	{
-		arr_arg[++cnt] = num_arg % div_arg;
-		num_arg /= div_arg;
-	}
-	arr_arg[0] = cnt;								//заносим количество цифр в числе
-	return cnt;
-}
-
-bool is_palindrom(char arr_arg[])
-//true - если число палиндром
-{
-	char ptr1 = 1;									//указатель(индекс) на первое число
-	char ptr2 = arr_arg[0];							//указатель(индекс) на последнее число
-	while (ptr1 < ptr2)
-	{
-		if (arr_arg[ptr1] == arr_arg[ptr2])
-		{
-			ptr1++;
-			ptr2--;
-		}
-		else
-			return false;
-	}
-	return true;
+	return a_arg * a_arg + b_arg * b_arg == c_arg * c_arg;
 }
 
 int main(void)
 {
-	double time_spent = 0.0; 										// для хранения времени выполнения кода
-	clock_t begin = clock(); 										// СТАРТ таймера
-
-	char arr_bin[22] = { 0 };						//массив для хранения цифр числа в 2-ичном виде
-	//первая цифра - количество цифр в занесенном числе, 1000000 -> 21-значное число в 2-ичной системе
-	char arr_dec[7] = { 0 };						//массив для хранения цифр числа в 10-тичном виде
 	int answ = 0;
+	int cnt_max = 0;
+	int cnt = 0;
 
-	for (int num = 1; num < 1000000; num++)
+	for (int p = 840; p <= 840; p++)
 	{
-		get_arr(arr_dec, num, 10);
-        get_arr(arr_bin, num, 2);
-		if (is_palindrom(arr_dec) && is_palindrom(arr_bin))
+		cnt = 0;
+		for (int c = p / 2; c > 0; c--)							// p = a + b + c => т.к. a + b > c => c < p/2
 		{
-			answ += num;
-			printf("%d\n", num);
+			int a_plus_b = p - c;								// a_plus_b = a + b > c
+			for (int a = a_plus_b; a >= a_plus_b / 2; a--)		// a и b равнозначны, после ab/2 решения будут повторяться
+				if (is_right_angle(a, a_plus_b - a, c))
+				{
+					printf("%d - %d - %d\n", a, a_plus_b - a, c);
+					cnt++;	
+				}
+						
 		}
-			
+
+		if (cnt_max < cnt)
+		{
+			cnt_max = cnt;
+			answ = p;
+		}
 	}
-
-	clock_t end = clock();								  	// СТОП таймера
-	time_spent += (double)(end - begin) / CLOCKS_PER_SEC; 	// время работы в секундах
-
-	printf("Ответ = %d время = %f\n", answ, time_spent);	// выводим результат и время работы программы
-	return 0;
+	printf("%d - %d", answ, cnt_max);											
+	return 0;	
 }
